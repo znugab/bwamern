ae0c5bf35b582bbbdb259c5c1224b3d1f8e91564
znugab <abintang07@gmail.com>
2020-05-02 14:36:53 +0700

    Initialize project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "assets/scss/style.scss";
+
+function App() {
+  return (
+    <div className="App">
+      hai dunia!
+    </div>
+  );
+}
+
+export default App;
